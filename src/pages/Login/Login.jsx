1a4d5c6473643f248eb799fb649538a34f7d1143
AmitjiMax <guptajiamit7766@gmail.com>
2026-02-24 00:00:11 +0530

    day5

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React,{ useState} from 'react'
 import './Login.css'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logos.png'
 
 const Login = () => {
+
+const[signState, setSignState]=useState("Sign In")
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt="" /> 
       <div className="login-form">
         <h1>Sign Up</h1>
         <form>
-          <input type="text" placeholder='Your Name' />
+          {signState==="Sign up"?<input type="text" placeholder='Your Name' />:<></>}
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
-          <button>Sign In</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -21,6 +24,12 @@ const Login = () => {
             <p>Need Help?</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signState==="Sign In"?
+          <p>New to Min Theater? <span onClick={()=>{setSignState("Sign Up")}}>Sigh Up Now </span></p>
+          :<p>Already have account? <span onClick={()=>{setSignState("Sign In")}}>Sigh In Now </span></p>
+          }
+        </div>
         </div>    
     </div>
   )
